3a50b6e1873b313677a253562c742b92945db230
Hju Kneyck Flores <hjucode@gmail.com>
2021-05-29 21:43:56 +0800

    refactor: reschedule get-main-assets

--- backend/gibme_crypto/celerybeat_schedule.py
+++ backend/gibme_crypto/celerybeat_schedule.py
@@ -9,7 +9,7 @@ CELERYBEAT_SCHEDULE = {
     },
     # General background tasks
     "get-main-assets": {
-        "schedule": crontab(minute=0, hour=0),
+        "schedule": crontab(minute=0, hour='*/3'),
         "task": "data.tasks.get_assets_main",
     }
 }
